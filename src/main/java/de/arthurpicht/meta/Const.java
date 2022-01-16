7f4ebb639af93c5895b8d21ff6d221d1f3cb173e
Arthur Picht <picht@arthurpicht.de>
2022-01-16 17:47:22 +0100

    version date updated

--- src/main/java/de/arthurpicht/meta/Const.java
+++ src/main/java/de/arthurpicht/meta/Const.java
@@ -3,9 +3,8 @@ package de.arthurpicht.meta;
 public class Const {
 
     public static final String META_CONF__FILE_NAME = "meta.conf";
-    public static final String META_DIR__ENV_VAR = "META_DIR";
 
     public static final String VERSION_TAG = "0.1-SNAPSHOT";
-    public static final String VERSION_DATE = "2022-01-06";
+    public static final String VERSION_DATE = "2022-01-16";
 
 }
