23457029ab38568a869fc76b9a02a5b891d2c33a
arthurpicht <info@arthurpicht.de>
2022-05-11 13:49:00 +0200

    dependency version update, prepare version 0.1

--- src/main/java/de/arthurpicht/meta/Const.java
+++ src/main/java/de/arthurpicht/meta/Const.java
@@ -4,7 +4,7 @@ public class Const {
 
     public static final String META_CONF__FILE_NAME = "meta.conf";
 
-    public static final String VERSION_TAG = "0.1-SNAPSHOT";
-    public static final String VERSION_DATE = "2022-01-16";
+    public static final String VERSION_TAG = "0.1";
+    public static final String VERSION_DATE = "2022-05-11";
 
 }
