9a3b656d9bfd6b2c31df33efadebd10430a55653
Arthur Picht <picht@arthurpicht.de>
2023-01-20 09:13:50 +0100

    version set to 0.2.2-SNAPSHOT

--- src/main/java/de/arthurpicht/meta/Const.java
+++ src/main/java/de/arthurpicht/meta/Const.java
@@ -4,7 +4,7 @@ public class Const {
 
     public static final String META_CONF__FILE_NAME = "meta.conf";
 
-    public static final String VERSION_TAG = "0.2.1";
+    public static final String VERSION_TAG = "0.2.2-SNAPSHOT";
     public static final String VERSION_DATE = "2023-01-20";
 
 }
