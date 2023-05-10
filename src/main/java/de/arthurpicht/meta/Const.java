95d1c4e3dafe64722c9097afb33308263c8ccbcc
Arthur Picht <picht@arthurpicht.de>
2023-05-10 10:18:11 +0200

    bug fix version 0.2.3 prepared

--- src/main/java/de/arthurpicht/meta/Const.java
+++ src/main/java/de/arthurpicht/meta/Const.java
@@ -4,7 +4,7 @@ public class Const {
 
     public static final String META_CONF__FILE_NAME = "meta.conf";
 
-    public static final String VERSION_TAG = "0.2.2-SNAPSHOT";
-    public static final String VERSION_DATE = "2023-01-20";
+    public static final String VERSION_TAG = "0.2.3";
+    public static final String VERSION_DATE = "2023-05-10";
 
 }
